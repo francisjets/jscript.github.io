f13d3d4ea3504c22be8b6f2fac46182e1b8e4e1f
francisco bravo <francisco.bravo.jerez@gmail.com>
2024-03-03 18:48:22 -0300

    creacion de un contador para javascript

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+let precioInicial = 40000;
+let cantidadInicial= 1;
+
+let precio= document.querySelector(".precio");
+let cantidad= document.querySelector(".cantidad");
+
+precio.innerHTML = precioInicial;
+cantidad.innerHTML = cantidadInicial;
+total.innerHTML=precioInicial;
+
+
+
+
+
+
+
